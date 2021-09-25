aff1f2986fe85a3d7567507b92630022815f0a66
IAmAngelLiu <b07705006@ntu.edu.tw>
2021-09-25 15:24:46 +0800

    add employee model

--- Diversity/accounts/models.py
+++ Diversity/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+	GENDER = (
+		('Female', 'Female'),
+		('Male', 'Male'),
+		('Others', 'Others'),
+		('Not disclosed', 'Not disclosed'),
+		)
+	RACE = (
+		('White', 'White'),
+		('Asian', 'Asian'),
+		('Black', 'Black'),
+		('American Indian', 'American Indian'),
+		('Hispanic', 'Hispanic'),
+		('Others', 'Others'),
+		)
+	FIRSTCOLLEGE = (
+		('Yes', 'Yes'),
+		('No', 'No'),
+		)
+	name = models.CharField(max_length=100, null=True)
+	gender = models.CharField(max_length=100, null=True, choices=GENDER)
+	race = models.CharField(max_length=100, null=True, choices=RACE)
+	firstcollege = models.CharField(max_length=100, null=True, choices=FIRSTCOLLEGE)
+	data_created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
+
+
+
